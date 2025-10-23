13d666ecf0f2d6a621cab7206300ff6d3bbae495
Tini Tran <tinit@stud.ntnu.no>
2025-10-23 13:09:50 +0200

    dropping columns and duplicates in movies

--- clean_insert.py
+++ clean_insert.py
@@ -37,6 +37,14 @@ class MoviePipeline:
         # Drop NaN values, as they should not exist in id,budget and revenue.
         df_movies = df_movies.dropna(subset=['id', 'budget', 'revenue'])
         df_movies[['id', 'budget', 'revenue']] = df_movies[['id', 'budget', 'revenue']].astype('int64')
+        df_movies = df_movies.sort_values(
+            by=['vote_count', 'popularity'],
+            ascending=[False, False]
+        )
+        # Drop duplicates based on movie ID, keeping the most recent one
+        df_movies = df_movies.drop_duplicates(subset='id', keep='first')
+
+        df_movies = df_movies.reset_index(drop=True)
         # Embedded columns
         cols = [
             "belongs_to_collection", "genres",
@@ -54,10 +62,11 @@ class MoviePipeline:
         df_movies['adult'] = df_movies['adult'].map({'False': False, 'True': True})
         df_movies['video'] = df_movies['video'].fillna(False).astype(bool)
         df_movies['release_date'] = pd.to_datetime(df_movies['release_date'], errors='coerce')
+        df_movies = df_movies.drop(['poster_path', 'homepage'], axis=1, errors='ignore')
         return df_movies
 
     def clean_links(self):
-        df_links = pd.read_csv("links_small.csv",)
+        df_links = pd.read_csv("links_small.csv", )
         # Drop links that are NaN
         df_links = df_links.dropna(subset=['movieId', 'tmdbId', 'imdbId'])
         # Drop duplicates
@@ -98,7 +107,7 @@ class MoviePipeline:
         ex_crew = ex_crew.dropna(subset=["crew"]).copy()
 
         ex_cast["cast"] = ex_cast["cast"].apply(lambda x: x if isinstance(x, dict) else None)
-        ex_cast= ex_cast.dropna(subset=["cast"]).copy()
+        ex_cast = ex_cast.dropna(subset=["cast"]).copy()
 
         # Normalize dicts to columns
         df_crew = pd.json_normalize(ex_crew["crew"])
